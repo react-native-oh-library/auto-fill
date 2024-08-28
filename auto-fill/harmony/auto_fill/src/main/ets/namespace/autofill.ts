6a97ae52f5c9a5ddbae72bd5bbcbf7bf26cc6484
longzi <zhengjinshouLL@163.com>
2024-08-28 15:34:40 +0800

    chore: Initializes the project directory structure (#1)
    Co-authored-by: zhengjinshou <>

---
+++ auto-fill/harmony/auto_fill/src/main/ets/namespace/autofill.ts
@@ -0,0 +1,20 @@
+
+import {
+    Descriptor as ComponentDescriptor,
+    ViewBaseProps,
+    ViewRawProps,
+    ViewDescriptorWrapperBase,
+    ColorValue,
+    Color,
+    RNInstance,
+    Tag,
+    RNComponentCommandReceiver,
+    ViewPropsSelector,
+  } from '@rnoh/react-native-openharmony/ts';
+  
+  
+  export namespace AutoFillTurboModule {
+    export const NAME = "AutoFillTurboModule" as const
+  
+  }
+  
